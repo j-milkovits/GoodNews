9b58445cbce99b9fa547dabe487942a127cdcf9d
j-milkovits <j.milkovits.t@posteo.net>
2024-06-02 12:29:14 +0200

    rewrite data loading code

--- get_data/get_imgs_only/get_images.py
+++ get_data/get_imgs_only/get_images.py
@@ -1,15 +1,11 @@
-import urllib2
+import argparse
 import json
 import os
-import tqdm
-import requests
-import unidecode
-import argparse
-import urlparse
 import posixpath
 import sys
-# from joblib import Parallel, delayed
-# from multiprocessing.dummy import Pool as ThreadPool
+import urllib.parse
+
+import requests
 from joblib import Parallel, delayed
 
 
@@ -21,21 +17,19 @@ def resolveComponents(url):
     'http://www.example.com/some/file.ext'
     """
 
-    parsed = urlparse.urlparse(url)
+    parsed = urllib.parse.urlparse(url)
     new_path = posixpath.normpath(parsed.path)
-    if parsed.path.endswith('/'):
+    if parsed.path.endswith("/"):
         # Compensate for issue1707768
-        new_path += '/'
+        new_path += "/"
     cleaned = parsed._replace(path=new_path)
 
     return cleaned.geturl()
 
+
 def retrieve_imgs(args):
-    #     for m in months:
-    # articles = json.load(open(root + "api/nyarticles_%s_%s.json" % (year, m), 'r'))
     thread_num, keys = args
     leftovers = []
-    # len_articles = len(articles['response']['docs'])
     len_images = len(keys)
 
     for num, key in enumerate(keys):
@@ -43,37 +37,41 @@ def retrieve_imgs(args):
             try:
                 img_url = resolveComponents(url)
                 img_data = requests.get(img_url, stream=True).content
-                with open(os.path.join("../../images/%s_%d.jpg" % (key, int(ix))), 'wb') as f:
+                with open(
+                    os.path.join("../../images/%s_%d.jpg" % (key, int(ix))), "wb"
+                ) as f:
                     f.write(img_data)
             except Exception as e:
                 leftovers.append(key)
-                print e, url
+                print(e, url)
 
-        sys.stdout.write('\r%d/%d text documents processed...' % (num, len_images))
+        sys.stdout.write("\r%d/%d text documents processed..." % (num + 1, len_images))
         sys.stdout.flush()
     if leftovers:
-        json.dump(leftovers, open(root + 'leftovers_%s' % thread_num, 'wb'))
+        with open(root + "leftovers_%s.json" % thread_num, "w") as f:
+            json.dump(leftovers, f)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    # Input paths
-    parser.add_argument('--num_thread', type=int, default=8,
-                        help='How many threads you want to use')
+    parser.add_argument(
+        "--num_thread", type=int, default=8, help="How many threads you want to use"
+    )
     opt = parser.parse_args()
-    hdr = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-        'Accept-Encoding': 'none',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'Connection': 'keep-alive'}
 
     root = "./"
     num_thread = opt.num_thread
-    leftovers = []
 
-    all_img_url = json.load(open('./img_urls_all.json'))
-    keys = all_img_url.keys()
-    thread_range = len(keys) / num_thread + 1
-    args = [(i + 1, keys[thread_range * i: thread_range * (i + 1)]) for i in xrange(num_thread)]
-    results = Parallel(n_jobs=num_thread, verbose=0, backend="loky")(map(delayed(retrieve_imgs), args))
+    with open("./img_urls_all.json") as f:
+        all_img_url = json.load(f)
+
+    keys = list(all_img_url.keys())
+    thread_range = len(keys) // num_thread + 1
+    args = [
+        (i + 1, keys[thread_range * i : thread_range * (i + 1)])
+        for i in range(num_thread)
+    ]
+
+    results = Parallel(n_jobs=num_thread, verbose=0, backend="loky")(
+        map(delayed(retrieve_imgs), args)
+    )
--- get_data/with_api/get_data_api.py
+++ get_data/with_api/get_data_api.py
@@ -1,99 +1,78 @@
-import urllib2
-from bs4 import BeautifulSoup
 import json
 import os
-import tqdm
+import posixpath
+from multiprocessing.dummy import Pool as ThreadPool
+from urllib.parse import urlparse, urlunparse
+
 import requests
 import unidecode
-import urlparse
-import multiprocessing
-import posixpath
-from goose import Goose
-import sys
-# from joblib import Parallel, delayed
+from bs4 import BeautifulSoup
+from goose3 import Goose
 from six.moves.html_parser import HTMLParser
-from multiprocessing.dummy import Pool as ThreadPool
+
 
 def resolveComponents(url):
-    """
-     resolveComponents('http://www.example.com/foo/bar/../../baz/bux/')
-    'http://www.example.com/baz/bux/'
-     resolveComponents('http://www.example.com/some/path/../file.ext')
-    'http://www.example.com/some/file.ext'
-    """
-
-    parsed = urlparse.urlparse(url)
+    parsed = urlparse(url)
     new_path = posixpath.normpath(parsed.path)
-    if parsed.path.endswith('/'):
-        # Compensate for issue1707768
-        new_path += '/'
+    if parsed.path.endswith("/"):
+        new_path += "/"
     cleaned = parsed._replace(path=new_path)
-    
-    return cleaned.geturl()
+    return urlunparse(cleaned)
+
 
 def get_soup(url):
-    req = urllib2.Request(url, headers=hdr)
-    # try:
-    page = urllib2.urlopen(req)
-    # except urllib2.HTTPError, e:
-    #     print num
-    #     print e.fp.read()
-    soup = BeautifulSoup(page, 'html.parser')
-    [s.extract() for s in soup('script')]
-    [s.extract() for s in soup('noscript')]
-#     img = soup.find_all('img')
+    req = requests.get(url, headers=hdr)
+    soup = BeautifulSoup(req.content, "html.parser")
+    [s.extract() for s in soup("script")]
+    [s.extract() for s in soup("noscript")]
     figcap = soup.find_all("figcaption")
     return soup, figcap
 
+
 def retrieve_articles(m):
-    
-#     for m in months:
-    articles = json.load(open(root + "api/nyarticles_%s_%s.json" % (year, m), 'r'))
+    articles = json.load(open(root + "api/nyarticles_%s_%s.json" % (year, m), "r"))
     month_data = {}
     leftovers = {}
-    len_articles = len(articles['response']['docs'])
-    for num, a in enumerate(articles['response']['docs']):
+    len_articles = len(articles["response"]["docs"])
+    for num, a in enumerate(articles["response"]["docs"]):
         try:
             data = {}
-            # using resolveComponents here, if some bad urls exist.
-            url = resolveComponents(a['web_url'])
+            url = resolveComponents(a["web_url"])
             extract = g.extract(url=url)
-            # Save the data before since we might save the data inside the if
-            data['headline'] = a.get('headline', None)
-            data['article_url'] = url
-            data['article'] = unidecode.unidecode(extract.cleaned_text)
-            data['abstract'] = a.get('abstract', None)
-    #             print url
-            if a['multimedia']:
-                data['images'] = {}
+            data["headline"] = a.get("headline", None)
+            data["article_url"] = url
+            data["article"] = unidecode.unidecode(extract.cleaned_text)
+            data["abstract"] = a.get("abstract", None)
+            if a["multimedia"]:
+                data["images"] = {}
                 soup, figcap = get_soup(url)
                 figcap = [c for c in figcap if c.text]
                 for ix, cap in enumerate(figcap):
-    #                     if im.attrs.get('data-mediaviewer-caption', 0):
-    #                     if cap.text:
-    #                     im = cap.find_previous('img')
-    #                     img_url = resolveComponents(im.attrs['src'])
-                    if cap.parent.attrs.get('itemid', 0):
-                        img_url = resolveComponents(cap.parent.attrs['itemid'])
-    #                     urllib.urlretrieve(img_url, root + "/images/%s_%d.%s" % (a['_id'], ix, img_url.split('.')[-1]))
+                    if cap.parent.attrs.get("itemid", 0):
+                        img_url = resolveComponents(cap.parent.attrs["itemid"])
                         img_data = requests.get(img_url, stream=True).content
-    #     , img_url.split('.')[-1]
-                        with open(os.path.join(root + "/images/%s_%d.jpg" % (a['_id'], ix)), 'wb') as f:
-    #                         shutil.copyfileobj(img_data, f)
+                        with open(
+                            os.path.join(root + "/images/%s_%d.jpg" % (a["_id"], ix)),
+                            "wb",
+                        ) as f:
                             f.write(img_data)
-    #                     data['images'].update({ix : im['data-mediaviewer-caption']})
-                        text = cap.get_text().split('credit')[0]
-                        text = text.split('Credit')[0]
-                        data['images'].update({ix : text})
-            sys.stdout.write('\r%d/%d text documents processed...' % (num, len_articles))
+                        text = cap.get_text().split("credit")[0]
+                        text = text.split("Credit")[0]
+                        data["images"].update({ix: text})
+            sys.stdout.write(
+                "\r%d/%d text documents processed..." % (num, len_articles)
+            )
             sys.stdout.flush()
-            month_data[a['_id']] = data
+            month_data[a["_id"]] = data
         except Exception as e:
-            leftovers[a['_id']] = a['web_url']
-            print e, url
-            
-    json.dump(month_data, open(data_root + 'nytimes_%d_%d.json' % (year, m), 'wb'))
-    json.dump(leftovers, open(data_root + 'leftovers_%d_%d.json' % (year, m), 'wb'))
+            leftovers[a["_id"]] = a["web_url"]
+            print(e, url)
+
+    with open(data_root + "nytimes_%d_%d.json" % (year, m), "w") as f:
+        json.dump(month_data, f)
+    with open(data_root + "leftovers_%d_%d.json" % (year, m), "w") as f:
+        json.dump(leftovers, f)
+
 
 def main(num_pool, months):
     pool = ThreadPool(num_pool)
@@ -101,24 +80,26 @@ def main(num_pool, months):
     pool.map(retrieve_articles, months)
     pool.close()
     pool.join()
-    json.dump(leftovers, open(root + "leftovers_%s.json" % year, 'w'))
+    with open(root + "leftovers_%s.json" % year, "w") as f:
+        json.dump(leftovers, f)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     hdr = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-        'Accept-Encoding': 'none',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'Connection': 'keep-alive'}
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+        "Accept-Encoding": "none",
+        "Accept-Language": "en-US,en;q=0.8",
+        "Connection": "keep-alive",
+    }
 
     h = HTMLParser()
     root = "./"
-    data_root = root + "data/"
+    data_root = os.path.join(root, "data/")
     g = Goose()
 
-    # This is a special case since 2018 had only 6 months by the time the data was getting collected.
-    # this year variable used in retrieve articles function
+    # Special case for 2018
     year = 2018
     main(6, range(1, 7))
 
@@ -128,3 +109,4 @@ if __name__ == '__main__':
     for y in reversed(years):
         year = y
         main(num_pool, months)
+
